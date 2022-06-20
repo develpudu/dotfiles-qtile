d7ec06574fecda532d4d1d6511f56c9c58bec5c8
JustineSmithies <justine@smithies.me.uk>
2022-06-20 20:36:41 +0100

    Resolved issue of getting number of connected screens under wayland

--- .config/qtile/screens.py
+++ .config/qtile/screens.py
@@ -3,6 +3,7 @@
 
 # Multimonitor support
 
+from libqtile import qtile
 from libqtile.config import Screen
 from libqtile import bar
 from libqtile.log_utils import logger
@@ -16,23 +17,8 @@ def status_bar(widgets):
 
 screens = [Screen(wallpaper='.cache/wallpaper', wallpaper_mode='fill', top=status_bar(primary_widgets))]
 
-# xrandr = "xrandr | grep -w 'connected' | cut -d ' ' -f 2 | wc -l"
-
-# command = subprocess.run(
-#     xrandr,
-#     shell=True,
-#     stdout=subprocess.PIPE,
-#     stderr=subprocess.PIPE,
-# )
-
-# if command.returncode != 0:
-#     error = command.stderr.decode("UTF-8")
-#     logger.error(f"Failed counting monitors using {xrandr}:\n{error}")
-#     connected_monitors = 1
-# else:
-#     connected_monitors = int(command.stdout.decode("UTF-8"))
-
-connected_monitors = 2
+connected_monitors = len(qtile.core.outputs)
+logger.warning(f"Found {connected_monitors} monitor(s)")
 
 if connected_monitors > 1:
     for _ in range(1, connected_monitors):
