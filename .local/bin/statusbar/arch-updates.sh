714ee30b0177fb07dda25c40384a3ca2524e94d8
JustineSmithies <justine@smithies.me.uk>
2022-06-21 21:56:08 +0100

    Updated arch-updates.sh script to allow keybind to operate the updates correctly

--- .local/bin/statusbar/arch-updates.sh
+++ .local/bin/statusbar/arch-updates.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 # Supress error codes to stop issues with CheckUpdates widget
-checkupdates 2> /dev/null
-paru -Qua 2> /dev/null
-exit 0
+#checkupdates 2> /dev/null
+#paru -Qua 2> /dev/null
+#exit 0
+
+case $1 in
+  key-update)
+		kitty -e paru
+		qtile cmd-obj -o widget checkupdates -f force_update
+    ;;
+  *)
+		# Supress error codes to stop issues with CheckUpdates widget
+		checkupdates 2> /dev/null
+		paru -Qua 2> /dev/null
+		exit 0
+    ;;
+esac
